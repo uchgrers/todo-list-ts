70bdf7e8963a020768467d98390ef335e179a9e0
uchgrers <97949004+uchgrers@users.noreply.github.com>
2025-01-25 19:22:40 +0300

    Update todosSlice.test.ts

--- src/redux/todosSlice.test.ts
+++ src/redux/todosSlice.test.ts
@@ -1,10 +1,1 @@
-import todosSlice, {initialState, setTodosCount} from './todosSlice';
 
-describe('todosSlice', () => {
-    it('should set todos count', function () {
-        const action = {type: setTodosCount.type, payload: 1}
-        const result = todosSlice(initialState, action)
-        console.log(result)
-        expect(result).toBe(1)
-    });
-})
